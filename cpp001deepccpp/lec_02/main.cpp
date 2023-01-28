98cd5b613cffe45ce5ea141e15b324c8c6135fcf
Iaroslav Os <osmnbx@gmail.com>
2023-01-28 20:14:48 +0200

    added/updated

--- cpp001deepccpp/lec_02/main.cpp
+++ cpp001deepccpp/lec_02/main.cpp
@@ -10,5 +10,18 @@ int main()
     int * numPointer = &num;
     cout << numPointer << "\n";
 
+    int ** numPointerPointer = &numPointer;
+    cout << numPointerPointer << "\n";
+
+    int *** numPointerPointerPointer = &numPointerPointer;
+    cout << numPointerPointerPointer << "\n";
+
+    *** numPointerPointerPointer = 222;
+    cout << num << "\n";    
+
+    int n = 1234;
+    int * nPtr = &n;
+    cout << nPtr << "\n";
+
     return 0;
 }
