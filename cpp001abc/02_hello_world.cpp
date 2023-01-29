660ed5f4fc1652771de43ed081fe37bd69cb2b5f
Iaroslav Os <osmnbx@gmail.com>
2023-01-29 11:54:38 +0200

    added/updated

--- cpp001abc/02_hello_world.cpp
+++ cpp001abc/02_hello_world.cpp
@@ -8,11 +8,9 @@ int main(int argc, const char * argv[])
 {
     cout << "hello world\n";
     
-    
     char name[25];
     cin.get(name, 25);
     cout << name << "\n";
-
 
     system("pause");
     return 0;
