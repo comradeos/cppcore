dd459ae16e369d6f6dfcaa59a49e41f0bfbfd158
Iaroslav Os <osmnbx@gmail.com>
2023-01-29 11:21:07 +0200

    added/updated

---
+++ cpp001abc/01_hello_world.cpp
@@ -0,0 +1,11 @@
+// g++ -o .\app .\01_hello_world.cpp ; .\app.exe ; del .\app.exe
+
+#include <iostream>
+
+using namespace std; 
+
+int main(int argc, const char * argv[])
+{
+    cout << "hello world\n";
+    return 0;
+}
