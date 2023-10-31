159065b005d8cb5c92d938c4125be0584543abf5
Iaroslav Os <osmnbx@gmail.com>
2023-10-31 11:28:02 +0200

    updated

---
+++ ___tmp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int square(int number) {
+    return number * number;
+}
+
+int main()
+{
+    printf("square ------> %s", square(3));
+    return 0;
+}
