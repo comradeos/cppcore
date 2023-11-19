a85691e20d2c3bc84ea758f96221ff4efcc098a9
Iaroslav Os <osmnbx@gmail.com>
2023-11-19 17:45:01 +0200

    updated

--- cpp005designPatterns/main.cpp
+++ cpp005designPatterns/main.cpp
@@ -2,10 +2,14 @@
 #include "modules/001_test.h"
 #include "modules/002_inheritance.h"
 #include "modules/003_glyph.h"
+#include "modules/004_AbstractFactory.h"
+#include "modules/005_Builder.h"
 
 int main() {
 //    testOop();
 //    InheritanceTest();
-    Test();
+//    Test();
+    AbstractFactoryTest();
+    BuilderTest();
     return 0;
 }
